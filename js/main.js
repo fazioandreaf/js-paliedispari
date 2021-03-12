9349a95d1246e18e18ccd4c87b156eed369e21a5
fazioandrea.f <fazioandrea.f@gmail.com>
2021-03-12 15:09:17 +0100

    Miglioramento del codice

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function es1() {
-    var parola= prompt('Scrivi la parola da esaminare');
+    var parola= prompt('Scrivi la parola da esaminare').toUpperCase();
     console.log(parola.length);
     var parolaArr=[];
     for (i=0;i<parola.length;i++) {
@@ -7,7 +7,6 @@ function es1() {
     }
     var condizione= parolaArr.length / 2;
     console.log(condizione)
-
     for (i=0;i<condizione;i++) {
         if(parolaArr[i]!=parolaArr[(parolaArr.length-i)-1]){
             return alert('La parola non é palindroma')
@@ -30,6 +29,11 @@ function es2() {
     } 
     console.log(scelta) 
     var numero= parseInt(prompt('Scegli un numero da 1 a 5'));
+    if(Number.isFinite(numero)!= true){
+        return alert( 'Non hai inserito un numero')
+    } else if(numero>5){
+        return alert( 'Non hai inserito un numero <5')
+    }
     var nPC= Math.floor(Math.random()*5); 
     console.log (nPC);
     var nTot= nPC + numero;
